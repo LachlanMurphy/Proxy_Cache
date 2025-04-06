3e1321ad3aff2506a0255d44b93a519e34b3c4f3
Lachlan Murphy <lachlan.g.murphy@gmail.com>
2025-04-06 17:05:36 -0600

    end to end transfer complete, TODO: cache

--- proxy.c
+++ proxy.c
@@ -24,9 +24,8 @@
 #include <fcntl.h>
 #include "array.h"
 
-#define BUFFERSIZE 2048
-#define INDEX "index.html"
-#define ROOT_DIR "./www"
+#define BUFFERSIZE 1024
+#define CAHCE_DIR "./cache"
 // HTTP version, status, content type, content length
 #define HEADER "%s %s\r\nContent-Type: %s\r\nContent-Length: %lu\r\n\r\n"
 #define FILE_SUF_LEN 8
@@ -167,118 +166,136 @@ void sigint_handler(int sig) {
 }
 
 void* socket_handler(void* arg) {
+    int bytes_read, port;
+    int server_socket;
+    struct sockaddr_in serveraddr;
+    struct hostent *server;
     socket_arg_t* args = (socket_arg_t *) arg;
     char buf[BUFFERSIZE];
+    char token_buf[BUFFERSIZE+8];
+    char* token;
     bzero(buf, BUFFERSIZE);
+    bzero(token_buf, BUFFERSIZE);
 
     // read in message
-    if (read(args->clientfd, buf, BUFFERSIZE) < 0) error("ERROR in reading from socket");
+    if ((bytes_read = read(args->clientfd, buf, BUFFERSIZE)) < 0) error("ERROR in reading from socket");
+    buf[bytes_read] = '\0';
     printf("%s", buf);
     
+    strncpy(token_buf, buf, BUFFERSIZE);
+    
     // parse message
-    char req[5][BUFFERSIZE / 2]; // req[0]=method ; req[1]=URI ; req[2]=version ; req[3]=hostname ; req[4]=body
-    char* tmp;
+    char req[4][BUFFERSIZE / 2]; // req[0]=method ; req[1]=URI ; req[2]=version ; req[3]=hostname
     int parse_err = 0;
     for (int i = 0; i < 3; i++) {
-        if (i == 0) tmp = strtok(buf, " ");
-        else tmp = strtok(NULL, " ");
+        if (i == 0) token = strtok(token_buf, " ");
+        else token = strtok(NULL, " ");
 
-        if (!tmp) {
+        if (!token) {
             parse_err = 1;
             break;
         }
-        memcpy(req[i], tmp, BUFFERSIZE);
+        memcpy(req[i], token, BUFFERSIZE);
     }
 
     // version may have a cairraige return on the end, replace with str terminator
     // also get the host name and port number
-    int line_brk = strcspn(req[2], "\n");
     if (!parse_err) {
-        req[2][line_brk] = '\0';
-        // 6 is the number of characters in the hostline that is not the actual host name
-        // eg: Host: www.hostname.com
-        char* host_line = buf + 9 + (line_brk + strlen(req[0]) + strlen(req[1])) * sizeof(char);
-        strncpy(req[3], host_line, strcspn(host_line, "\r"));
-        req[3][strcspn(req[3], "\r")] = '\0';
-        char* body_ptr = strstr(buf, "\r\n\r\n")+4;
+        req[2][strcspn(req[2], "\r")] = '\0';
 
-        // TODO: seg fault happening with strcspn
-        if (strcspn(body_ptr, "\0") == 0) {
-            req[4][0] = '\0';
-        } else {
-            strncpy(req[4], body_ptr, strcspn(body_ptr, "\0"));
+
+        strncpy(token_buf, buf, BUFFERSIZE);
+        token = strtok(token_buf, "\r\n");
+        while (token) {
+            if (!strncmp(token, "Host: ", strlen("Host: "))) {
+                strncpy(req[3], token+strlen("Host: "), BUFFERSIZE);
+                break;
+            } else {
+                token = strtok(NULL, "\r\n");
+            }
         }
-        printf("hostname: %s ; %s\n", req[3], req[4]);
-        // printf("String in hex: ");
-        // for (size_t i = 0; i < strlen(host_line); i++) {
-        //     printf("%02x ", (unsigned char)host_line[i]);
-        // }
-        // printf("\n");
+
+        if (!token) parse_err = 1;
+
+        // get port number
+        int colon = strcspn(req[3], ":");
+        if (colon != strlen(req[3])) {
+            port = atoi(req[3]+colon+1);
+        } else {
+            port = 80;
+        }
+        req[3][colon] = '\0';
+
+        // now that we have the host, we can extract the URI
+        char* path = strcspn(req[1], "http://")+req[1]+strlen("http://");
+        path += strcspn(path, "/");
+        int len = strlen(path);
+        strncpy(req[1], path, strlen(path));
+        req[1][len] = '\0';
+
     }
-    exit(0);
+    
 
 
-    if (parse_err) {
+    if (parse_err || strncmp(req[0], "GET", strlen("GET"))) {
         // 400 Bad request
         sprintf(buf, HEADER, "HTTP/1.1", "400 Bad Request", "text/plain", 0UL);
         if (send(args->clientfd, buf, strlen(buf), 0) < 0) error("ERROR in send");
-    } else if (strncmp(req[0], "GET", strlen("GET"))) { // Check method, only get is allowed
-        // 405 Method Not Allowed
-        sprintf(buf, HEADER, req[2], "405 Method Not Allowed", "text/plain", 0UL);
-        if (send(args->clientfd, buf, strlen(buf), 0) < 0) error("ERROR in send");
-    } else if (strncmp("HTTP/1.0", req[2], strlen(req[2]) - 1) &&
-            strncmp("HTTP/1.1", req[2], strlen(req[2]) - 1)) { // check HTTP version
-        // 505 HTTP Version Not Supported
-        sprintf(buf, HEADER, req[2], "505 HTTP Version Not Supported", "text/plain", 0UL);
-        if (send(args->clientfd, buf, strlen(buf), 0) < 0) error("ERROR in send");
     } else {
-        char file_name[BUFFERSIZE];
-        strncpy(file_name, ROOT_DIR, strlen(ROOT_DIR)+1);
-        strcat(file_name, req[1]);
-
-        // if we are requesting a directory, replace ending with index.html
-        if (file_name[strlen(file_name)-1] == '/') {
-            // char indx_suffix
-            strcat(file_name, (char *) &INDEX);
+        // connect to server and request data
+        if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) error("server socket");
+        
+        /* gethostbyname: get the server's DNS entry */
+        server = gethostbyname(req[3]);
+        if (server == NULL) {
+            fprintf(stderr,"ERROR, no such host as %s\n", req[3]);
+            exit(0);
         }
 
-        // attempt to access file requested
-        if (!access(file_name, F_OK) && !access(file_name, R_OK)) {
-            // file exists
+        /* build the server's Internet address */
+        bzero((char *) &serveraddr, sizeof(serveraddr));
+        serveraddr.sin_family = AF_INET;
+        bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+        serveraddr.sin_port = htons(port);
 
-            // send file
-            FILE* file = fopen(file_name, "r");
+        if (connect(server_socket, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) error("connecting to server");
 
-            // get file size
-            fseek(file, 0, SEEK_END);
-            unsigned long file_size = ftell(file);
-            fseek(file, 0, SEEK_SET);
+        // make new HTTP request
+        char* headers = strcspn(buf, "\r\n")+2+buf;
+        sprintf(token_buf, "GET %s %s\r\n", req[1], req[2]);
+        // sprintf(token_buf, "GET %s %s\r\n", req[1], req[2]);
+        strcat(token_buf, headers);
 
-            // send the file header
-            sprintf(buf, HEADER, req[2], "200 OK", file_types[find_file_type(file_name)], file_size);
-            if (send(args->clientfd, buf, strlen(buf), 0) < 0) error("ERROR in send");
+        if (send(server_socket, token_buf, BUFFERSIZE, 0) < 0) error("ERROR in send");
 
-            // send file content
-            off_t offset = 0;
-            int n = 0;
-            while (n < file_size) {
-                n = sendfile(args->clientfd, fileno(file), &offset, file_size);
-                if (n < 0) error("ERROR in sendfile");
-            }
-            fclose(file);
-        } else {
-            // check whether it was a permission err or file doesn't exist
-            if (access(file_name, F_OK)) {
-                // 404 file not found
-                sprintf(buf, HEADER, req[2], "404 Not Found", "text/plain", 0UL);
-                if (send(args->clientfd, buf, strlen(buf), 0) < 0) error("ERROR in send");
-            } else if (access(file_name, R_OK)) {
-                // 403 forbidden
-                sprintf(buf, HEADER, req[2], "403 Forbidden", "text/plain", 0UL);
-                if (send(args->clientfd, buf, strlen(buf), 0) < 0) error("ERROR in send");
+        int n;
+        // send initial meta data
+        if ((n = recv(server_socket, buf, BUFFERSIZE, 0)) < 0) error("ERROR in recv");
+        if (send(args->clientfd, buf, n, 0) < 0) error("ERROR in send");
+        
+        // get content length
+        int content_length = 0;
+        strncpy(token_buf, buf, BUFFERSIZE);
+        token = strtok(token_buf, "\r\n");
+        while (token) {
+            if (!strncmp(token, "Content-Length: ", strlen("Content-Length: "))) {
+                content_length = atoi(token+strlen("Content-Length: "));
+                break;
+            } else {
+                token = strtok(NULL, "\r\n");
             }
         }
 
+        int content_rec = 0;
+        while (content_rec < content_length) {
+            
+            if ((n = recv(server_socket, buf, BUFFERSIZE, 0)) < 0) error("ERROR in recv");
+            content_rec += n;
+            if (n == 0) break;
+
+            // send response back to client
+            if (send(args->clientfd, buf, n, 0) < 0) error("ERROR in send1");
+        }
     }
 
     // socket no longer needed
